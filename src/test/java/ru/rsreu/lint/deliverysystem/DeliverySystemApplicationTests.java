182a1e37e8033b349461687d4945a2d35259b1d6
vlint <lintvadim@gmail.com>
2025-05-16 12:05:00 +0300

    Классы из init commit

--- src/test/java/ru/rsreu/lint/deliverysystem/DeliverySystemApplicationTests.java
+++ src/test/java/ru/rsreu/lint/deliverysystem/DeliverySystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeliverySystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
